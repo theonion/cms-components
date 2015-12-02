bf71679e8074a23b64931f492cb3235eea754859
collin <collintmiller@gmail.com>
2015-12-02 15:24:56 -0600

    Only have a title for cms-input

--- components/cms-input/cms-input.js
+++ components/cms-input/cms-input.js
@@ -6,33 +6,9 @@ angular.module('cmsComponents')
       templateUrl: 'components/cms-input/cms-input.html',
       restrict: 'E',
       scope: {
-        title: '@',
-        placeholder: '@',
-        type: '@',
-        multiline: '@',
-        select: '@',
-        rows: '@',
-        ngModel: '=',
-        name: '@',
+        title: '@'
       },
       transclude: true,
-      compile: function (element, attributes) {
-        return {
-          pre: function ($scope, $element, $attributes) {
-            if ($attributes.reverseTrueAndFalse !== undefined) {
-              $scope.reverse = true;
-            }
-            if ($attributes.multiline !== undefined) {
-              $scope.multiline = true;
-            }
-          }
-        }
-      },
-      link: function ($scope, $element, $attrs) {
-        if ($attrs.type === 'select') {
-          $attrs.select = 'select';
-        }
-      },
       controller: ['$scope', '$controller', function ($scope, $controller) {
       }]
     }
