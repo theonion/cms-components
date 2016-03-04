759ea4e82296eec3a1a192fc0dc91f3e7b235917
Andrew Kos <akos123@gmail.com>
2016-03-04 12:48:10 -0600

    tests for cms-nav-user

--- components/cms-nav-user/cms-nav-user.js
+++ components/cms-nav-user/cms-nav-user.js
@@ -2,32 +2,24 @@
 
 angular.module('cmsComponents.navUser', [
   'cmsComponents.auth.user',
-  'cmsComponents.filters.userDisplay'
+  'cmsComponents.auth.loginRequiredWrapper',
+  'cmsComponents.filters.userDisplay',
+  'cmsComponents.templates'
 ])
-  .directive('cmsNavUser', function () {
-    return {
-      templateUrl: 'components/cms-nav-user/cms-nav-user.html',
-      restrict: 'E',
-      controller: [
-        '$rootScope', '$scope', 'CurrentUser',
-        function ($rootScope, $scope, CurrentUser) {
-
-          var onLogin = function (user) {
-            $scope.user = user;
-          };
-
-          var onLogout = function () {
-            $scope.user = null;
-          };
-
-          CurrentUser.addLoginHandler(onLogin);
-          CurrentUser.addLogoutHandler(onLogout);
-
-          $scope.$on('$destroy', function () {
-            CurrentUser.removeLoginHandler(onLogin);
-            CurrentUser.removeLogoutHandler(onLogout);
-          });
-        }
-      ]
+  .directive('cmsNavUser', [
+    function () {
+      return {
+        templateUrl: 'components/cms-nav-user/cms-nav-user.html',
+        restrict: 'E',
+        controller: [
+          '$scope', 'CurrentUser',
+          function ($scope, CurrentUser) {
+            CurrentUser.$get()
+              .then(function (user) {
+                $scope.user = user;
+              });
+          }
+        ]
+      }
     }
-  });
+  ]);
---
+++ components/cms-nav-user/cms-nav-user.spec.js
@@ -0,0 +1,70 @@
+'use strict';
+
+describe('Directive: cms-nav-user', function () {
+  require('./cms-nav-user');
+
+  var $q;
+  var CurrentUser;
+  var digestedScope;
+  var html;
+
+  beforeEach(function () {
+    angular.mock.module('cmsComponents.auth.user');
+    angular.mock.module('cmsComponents.auth.loginRequiredWrapper');
+    angular.mock.module('cmsComponents.filters.userDisplay');
+    angular.mock.module('cmsComponents.navUser');
+
+    inject(function (_$q_, _CurrentUser_, $compile, $rootScope) {
+      $q = _$q_;
+      CurrentUser = _CurrentUser_;
+
+      html = angular.element('<cms-nav-user></cms-nav-user>');
+
+      digestedScope = function () {
+        var $directiveScope = $rootScope.$new();
+        var element = $compile(html)($directiveScope);
+        $directiveScope.$digest();
+        return element.scope();
+      };
+    });
+  });
+
+  describe('display conditions', function () {
+
+    it('should not display if there is no user', function () {
+      CurrentUser.$get = sinon.stub().returns($q.reject());
+      digestedScope();
+
+      expect(html.find('cms-login-required-wrapper').children().length).to.equal(0);
+    });
+
+    it('should display if there is a user', function () {
+      var user = {};
+
+      CurrentUser.$get = sinon.stub().returns($q.resolve(user));
+      digestedScope();
+
+      expect(html.find('cms-login-required-wrapper').children().length).to.be.above(0);
+    });
+  });
+
+  describe('markup', function () {
+
+    it('should display user name based on userDisplay filter', function () {
+      var user = {full_name: 'garbage'};
+
+      CurrentUser.$get = sinon.stub().returns($q.resolve(user));
+      digestedScope().$digest();
+
+      expect(html.html().match(user.full_name).length).to.equal(1);
+    });
+
+    it('should have a link to logout route', function () {
+
+      CurrentUser.$get = sinon.stub().returns($q.resolve({}));
+      digestedScope().$digest();
+
+      expect(html.find('a').attr('ui-sref')).to.equal('logout');
+    });
+  });
+});
